9564f1e3a74d57851dee4f034a78cd43bac5c374
JakeHenderson21 <23626171@stu.mmu.ac.uk>
2024-03-04 15:50:06 +0000

    Wave System Prototype Implemented
    The wave system where enemies will now spawn in wave as well as every 4 round introducing a new type of enemy is now implemented but will potentially need adjusted later on.

--- Assets/Scripts/EnemySpawnerController.cs
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -14,13 +14,16 @@ public class EnemySpawnerController : MonoBehaviour
     public Boolean waveComplete;
     public int waveNumber;
     private Canvas canvas;
+    [SerializeField]private Boolean startOfRound = true;
     private int rng;
-    private int timeBetweenSpawn = 3;
+    private int timeBetweenSpawn = 2;
+   [SerializeField] private float timeCounter;
     private int numberOfEnemies = 10;
-    public List<string> eneimes = new List<string>();
-    public List<string> usableEneimes = new List<string>();
-    [SerializeField] private List<string> unusedEneimes = new List<string>();
+    public List<GameObject> eneimes = new List<GameObject>();
+    public List<GameObject> usableEneimes = new List<GameObject>();
+    [SerializeField] private List<GameObject> unusedEneimes = new List<GameObject>();
     private Vector3 location;
+    private int counter;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,41 +31,10 @@ public class EnemySpawnerController : MonoBehaviour
         lane1 = GameObject.Find("EnemyLane1").GetComponent<Transform>();
         lane2 = GameObject.Find("EnemyLane2").GetComponent<Transform>();
         lane3 = GameObject.Find("EnemyLane3").GetComponent<Transform>();
-        while(waveNumber <= 50)
-        {
-            if (waveComplete == false && GameObject.FindGameObjectsWithTag("Enemy").Length >= 0)
-            {
-
-                StartCoroutine(createEnemy());
-            }
-            if (waveComplete == true)
-            {
-                waveComplete = false;
-                numberOfEnemies += 2;
-                waveNumber++;
-            }
-            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
-            {
-                waveComplete = true;
-            }
-        }
-     
-        
+        EnemySelector();
     }
-    private IEnumerator createEnemy()
-        {
-        for (int i = 1; i < numberOfEnemies; i++)
-        {
-            yield return new WaitForSeconds(timeBetweenSpawn);
-            enemy = Instantiate(enemyType, gameObject.transform);
-            enemyCanvas = enemy.GetComponent<StandardEnemy>();
-            enemy.transform.SetParent(canvas.transform, false);
-            enemyCanvas.canvas = canvas;
-            rngSelector();
-            enemy.transform.position = location;       
-        }
-        
-    }
+  
+ 
     private void rngSelector()
     {
         rng = UnityEngine.Random.Range(1, 4);
@@ -81,13 +53,63 @@ public class EnemySpawnerController : MonoBehaviour
     {
         for(int i = 0; i < numberOfEnemies;i++)
         {
-            int random = UnityEngine.Random.Range(1, usableEneimes.Capacity + 1);
+            int random = UnityEngine.Random.Range(0, usableEneimes.Count);
+            Debug.Log(random);
             eneimes.Add(usableEneimes[random]);
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeCounter > 0)
+        {
+            timeCounter -= Time.deltaTime;
+
+        }
+        else
+        {
+            timeCounter = timeBetweenSpawn;
+            if(eneimes.Count > 0)
+            {
+            
+                try
+                {
+                    enemy = Instantiate(eneimes[0], gameObject.transform);
+                    eneimes.Remove(eneimes[0]);
+                    startOfRound = false;
+                    enemyCanvas = enemy.GetComponent<StandardEnemy>();
+                    enemy.transform.SetParent(canvas.transform, false);
+                    enemyCanvas.canvas = canvas;
+                    rngSelector();
+                    enemy.transform.position = location;
+                    counter++;
+                }
+                catch (Exception)
+                {
+
+                }
+                   
+            
+            }
+        }
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length <= 0 && startOfRound == false && eneimes.Count == 0)
+        {
+            waveComplete = true;
+        }
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0 && waveComplete == true)
+        {                
+            float wave = waveNumber % 3;
+            Debug.Log("Wave Value: " + wave);
+            if (wave == 0)
+            {
+                usableEneimes.Add(unusedEneimes[0]);
+                unusedEneimes.Remove(unusedEneimes[0]);
+            }
+            waveComplete = false;
+            startOfRound = true;
+            numberOfEnemies += 2;
+            waveNumber++;
+        EnemySelector();
+        }
     }
 }
