da6c6f8134c2995d896c0cc04eefa6e083d600f9
whirlstrike <c.c.j.w.cw@gmail.com>
2024-03-12 23:55:43 +0000

    music and SFX volume controls
    adding the ability to change the volume of music and SFX in the settings menu
    
    also added two different audio mixers for SFX and music whenever that gets added.

--- Assets/Scripts/menu_scripts.cs
+++ Assets/Scripts/menu_scripts.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class menu_scripts : MonoBehaviour
 {
+
+    public AudioMixer mixer;
+
     public void playgame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//when adding this to the main game insure that the game scense is set to 1 on the build settings 
@@ -20,4 +24,17 @@ public class menu_scripts : MonoBehaviour
        Application.Quit();
         Debug.Log("quit game");//quits the game 
     }
+
+    public void setmusic (float musicvolume)
+    {
+        Debug.Log(musicvolume);//checks to see if volume slider has been chainged
+        mixer.SetFloat("music", musicvolume);
+    }
+
+    public void setSFX(float SFXvolume)
+    {
+        Debug.Log(SFXvolume);//checks to see if volume slider has been chainged
+        mixer.SetFloat("SFX", SFXvolume);
+    }
+
 }
